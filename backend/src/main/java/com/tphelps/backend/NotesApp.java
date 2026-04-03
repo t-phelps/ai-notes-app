1f2071b711bc47420325f53f4fd79ec2cd7e1263
Timothy Phelps <tdp0013@mix.wvu.edu>
2026-04-02 20:51:12 -0400

    process builder for rclone on app startup:

--- backend/src/main/java/com/tphelps/backend/NotesApp.java
+++ backend/src/main/java/com/tphelps/backend/NotesApp.java
@@ -1,16 +1,39 @@
 package com.tphelps.backend;
 
+import jakarta.annotation.PostConstruct;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import java.io.IOException;
+
 @SpringBootApplication
 @EnableScheduling
 public class NotesApp {
 
+    private static final Logger logger = LoggerFactory.getLogger(NotesApp.class);
 	public static void main(String[] args) {
-		SpringApplication.run(NotesApp.class, args);
+
+        SpringApplication.run(NotesApp.class, args);
 	}
+
+    @PostConstruct
+    public void startRclone(){
+        try{
+            ProcessBuilder processBuilder = new ProcessBuilder(
+                    "rclone", "rcd",
+                    "--rc-no-auth",
+                    "--rc-addr", "127.0.0.1/5572"
+            );
+            processBuilder.redirectErrorStream(true);
+            processBuilder.start();
+            logger.info("RCLone started");
+        }catch(IOException e){
+            logger.error("Failed to start rclone on app startup: {}", e.getMessage());
+        }
+    }
 
 }
