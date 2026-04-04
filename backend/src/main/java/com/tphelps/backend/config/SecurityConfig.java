2521d433348231eacf82f26ed5a29ed6d814fece
Timothy Phelps <tdp0013@mix.wvu.edu>
2026-04-04 11:39:55 -0400

    azure workflow test

--- backend/src/main/java/com/tphelps/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/tphelps/backend/config/SecurityConfig.java
@@ -24,7 +24,6 @@ import java.util.List;
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
-
     private final JwtAuthEntryPoint jwtAuthEntryPoint;
 
     @Value("${front.end.url}")
